b75be16d2e219fe9700f775373ce62a82fa7624c
Ahmed Elzarzamony <ahmed.mrcode@gmail.com>
2019-03-14 22:48:45 +0200

    handle router class

---
+++ Core/Router.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Router
+{
+    protected $routes = [];
+
+    public function add($route, $params)
+    {
+        $this->routes[$route] = $params;
+    }
+
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -4,6 +4,16 @@
  * 
  *  Front Controller
  * 
+ * echo "Requested Query = " . $_SERVER['QUERY_STRING'];
  */
 
- echo "Requested Query = " . $_SERVER['QUERY_STRING'];
+ require '../Core/Router.php';
+
+ $router = new Router();
+ //echo get_class($router);
+
+ $router->add('', ['controller' => 'Home', 'method' => 'index']);
+ $router->add('posts', ['controller' => 'Posts', 'method' => 'index']);
+ $router->add('posts/new', ['controller' => 'Posts', 'method' => 'new']);
+
+ echo '<pre>', print_r($router->getRoutes());
