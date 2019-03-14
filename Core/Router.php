b73616fdd1b6ca177b82f9c9799622a2a4abcd0d
Ahmed Elzarzamony <ahmed.mrcode@gmail.com>
2019-03-15 00:46:59 +0200

    Router class: update match method by using regular expression

--- Core/Router.php
+++ Core/Router.php
@@ -17,11 +17,24 @@ class Router
 
     public function match($url)
     {
-        foreach($this->routes as $router => $params){
-            if($url == $router){
-                $this->params = $params;
-                return true;
+        // foreach($this->routes as $router => $params){
+        //     if($url == $router){
+        //         $this->params = $params;
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        $rg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+        if(preg_match($rg_exp, $url, $matches)){
+            $params = [];
+            foreach($matches as $key=>$match){
+                if(is_string($key)){
+                    $params[$key] = $match;
+                }
             }
+            $this->params = $params;
+            return true;
         }
         return false;
     }
