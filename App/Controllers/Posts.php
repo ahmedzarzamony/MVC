8085ce616b9eb7620ca4954961c1a0b8dbacf3bb
Ahmed Elzarzamony <ahmed.mrcode@gmail.com>
2019-03-15 17:30:01 +0200

    add home and posts controllers

---
+++ App/Controllers/Posts.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Controllers;
+
+class Posts
+{
+    public function index()
+    {
+        echo "Posts - index";
+    }
+
+    public function addNew()
+    {
+        echo "Posts - add New";
+    }
+}
